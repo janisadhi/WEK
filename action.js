648dafc6393b4440e976121a7858509848ac5e18
Janis Adhikari <adhikarijanis@gmail.com>
2023-04-03 12:14:11 +0545

    menu js

--- action.js
+++ action.js
@@ -2,4 +2,15 @@
         function back() {
             window.alert("THis is not connected to backend \n\n\n\nNOTE:this website is only made for educational purpose");
         }
-    
+    x=document.getElementById("middle")
+    y=document.getElementById("navbar")
+    function menu(){
+        if(x.style.display==="flex" && y.style.height==="170px"){
+            x.style.display="none";
+            y.style.height="70px";
+        }
+        else{
+            x.style.display="flex";
+            y.style.height="170px"
+        }
+    }
